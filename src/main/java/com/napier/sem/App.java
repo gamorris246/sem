f30cc88d59bb562f54ec81c0832e6028218226a4
Greg Morris <40498429@live.napier.ac.uk>
2022-02-13 19:10:42 -0400

    Create new branch getDepartment

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -248,10 +248,9 @@ public class App{
             String strSelect =
                 "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
                     + "FROM employees, salaries, dept_emp, departments "
-                    + "WHERE employees.emp_no = salaries.emp_no "
+                    + "WHERE employees.dept = " + dept_name;
                     + "AND employees.emp_no = dept_emp.emp_no "
                     + "AND dept_emp.dept_no = departments.dept_no "
-                    + "AND salaries.to_date = '9999-01-01' "
                     + "AND departments.dept_no = '<dept_no>' "
                     + "ORDER BY employees.emp_no ASC ";
 
